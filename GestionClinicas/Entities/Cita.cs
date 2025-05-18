1c903294a46152b11540274eec7ef830efcbc077
LordCasta <castanocastano6@gmail.com>
2025-05-18 15:56:26 -0500

    Se añade la entidad Cita

---
+++ GestionClinicas/Entities/Cita.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionClinicas.Entities
+{
+    public class Cita
+    {
+        int CitaID { get; set; }
+        int PacienteID { get; set; }
+        int DoctorID { get; set; }
+        DateTime Fecha { get; set; }
+        DateTime Hora { get; set; }
+        string Estado { get; set; } // Pendiente, Completada, Cancelada
+    }
+}
