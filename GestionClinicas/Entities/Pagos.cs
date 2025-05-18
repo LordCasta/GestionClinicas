7dd2f2acaf53ff0bea01a2376a0fe7dda220087e
LordCasta <castanocastano6@gmail.com>
2025-05-18 16:07:54 -0500

    Se añade entidad de Pagos

---
+++ GestionClinicas/Entities/Pagos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionClinicas.Entities
+{
+    public class Pagos
+    {
+        public int PagoID { get; set; }
+        public int TratamiendoID { get; set; }
+        public decimal Monto { get; set; }
+        public DateTime FechaPago { get; set; }
+        public string MetodoPago { get; set; } // Efectivo, Tarjeta, Transferencia
+    }
+}
