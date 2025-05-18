cff77a97295184314748f75a0b4922f9ce9b2b24
LordCasta <castanocastano6@gmail.com>
2025-05-18 16:13:29 -0500

    Se añade entidad Tratamientos

---
+++ GestionClinicas/Entities/Tratamientos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionClinicas.Entities
+{
+    public class Tratamientos
+    {
+        public int TratamientoID { get; set; }
+        public int PacienteID { get; set; }
+        public string TipoTratamiento { get; set; } 
+        public DateTime FechaInicio { get; set; }
+        public int Duracion { get; set; } // en días
+        public decimal CostoTotal { get; set; }
+        public decimal SaldoPendiente { get; set; }
+    }
+}
