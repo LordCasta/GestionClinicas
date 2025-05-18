3b1e58cc4f54feba1fce8a2c8b977bf5b9d3ddb1
LordCasta <castanocastano6@gmail.com>
2025-05-18 15:51:53 -0500

    Se añade la entidad Doctor

---
+++ GestionClinicas/Entities/Doctor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionClinicas.Entities
+{
+    public class Doctor
+    {
+        public int DoctorID { get; set; }
+        public string Nombre { get; set; }
+        public string Especializacion { get; set; }
+        public string Telefono { get; set; }
+    }
+}
