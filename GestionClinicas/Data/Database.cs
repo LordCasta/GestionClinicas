937bef71b01b4250cae66fdaca78cacb468c529f
LordCasta <castanocastano6@gmail.com>
2025-05-18 01:33:55 -0500

    Se añade clase datos para la base de datos

---
+++ GestionClinicas/Data/Database.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace GestionClinicas.Data
+{
+    public class Database
+    {
+        private static SqlConnection _connection;
+
+        public static SqlConnection GetConnection()
+        {
+            if (_connection == null)
+            {
+                string connStr = ConfigurationManager.ConnectionStrings["SqlConn"].ConnectionString;
+                _connection = new SqlConnection(connStr);
+            }
+            return _connection;
+        }
+    }
+}
