d11f531fbf310d355a021ab30d208d09c83eb856
LordCasta <castanocastano6@gmail.com>
2025-05-18 16:03:09 -0500

    Se añade entidad para Horario de los Doctores (tabla)

---
+++ GestionClinicas/Entities/HorarioDoctores.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionClinicas.Entities
+{
+    public class HorarioDoctores
+    {
+        public int HorarioID { get; set; }
+        public int DoctorID { get; set; }
+        public int Dia { get; set; } // 1: Lunes, 2: Martes, ..., 7: Domingo
+        public TimeSpan HoraInicio { get; set; }
+        public TimeSpan HoraFin { get; set; }
+    }
+}
